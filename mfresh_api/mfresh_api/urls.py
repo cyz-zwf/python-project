631c22109320bbb2e84d6d2ccc3dfd91ac1b7cf4
cyz <cyz@tedu.cn>
2019-09-20 15:48:52 +0800

    测试

--- mfresh_api/mfresh_api/urls.py
+++ mfresh_api/mfresh_api/urls.py
@@ -31,26 +31,42 @@ def testAdd(req):
     return JsonResponse({})
 
 #测试:向数据库中删除一条记录
-def testdelete():
-    pass
-#测试:向数据库中修改一条记录
-def testupdate():
-    pass
-#测试:查询所有记录
-def testAll():
-    pass
-#测试:查询一行记录
-def testone():
-    pass
+def testdelete(req):
+    r = MfUser.objects.filter(uid=2).delete()
+    print(r,type(r))
+    return JsonResponse({})
 
+
+#测试:向数据库中修改一条记录
+def testupdate(req):
+    upda = MfUser.objects.filter(uid=3).update(uname='yaya',upwd='22')
+    print(upda,type(upda))
+    return JsonResponse({})
+
+
+#测试:查询所有记录
+def testAll(req):
+    all = MfUser.objects.values('uid','uname','upwd')
+    #all = all[5:10] 可以对下标进行分页
+    print(all,type(all))  #返回值是: QuerySet  对象
+    #把QuerySet强制转换为dict或则list  就可以使用json转换
+    all = list(all)
+    return JsonResponse(all,safe=False )
+
+#测试:查询一行记录
+def testone(req):
+    one = MfUser.objects.filter(uname='yaya').values('uid','uname','upwd')
+    one = one[0]   #<class 'dict'>
+    print(one,type(one))  #返回值是: QuerySet  对象
+    return JsonResponse(one,safe=False)
 
 
 urlpatterns = [
     path('test/add',testAdd), #测试
-    #path('test/testdelete',testdelete),
-    #path('test/testupdate',testupdate),
-    #path('test/testAll',testAll),
-    #path('test/testone',testone), 
+    path('test/testdelete',testdelete),
+    path('test/testupdate',testupdate),
+    path('test/testAll',testAll),
+    path('test/testone',testone), 
 
     path('admin/', admin.site.urls),
     path('user/login',uv.userLogin), #用户模块
