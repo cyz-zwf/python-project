83473a8a8b40576621530f53d9024e63ca6bf073
cyz <cyz@tedu.cn>
2019-09-20 16:14:13 +0800

    用户登录验证

--- mfresh_api/user/views.py
+++ mfresh_api/user/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from user.models import MfUser
 # Create your views here.
 
 #用户登录验证
 def userLogin( req ):
-    output = {'code':200}
+    #获取查询字符串的请求参数
+    n = req.GET['unameOrPhone']
+    p = req.GET['upwd']
+    #执行数据库查询
+    #SELECT uid FROM mf_user
+    #WHERE (uname=? AND upwd=?) OR (phone=? AND upwd=?)
+    result = MfUser.objects.filter(uname=n,upwd=p) | MfUser.objects.filter(phone=n,upwd=p)
+    if len(result)>0:  #查到了相关记录
+        user = result[0]
+        output = {'code':1,'uid':user.uid,'uname':user.uname,'phone':user.phone}
+    else :              #没查到相关记录
+        output = {'code':400}
+
     res = JsonResponse(output,safe=False)
     res['Access-Control-Allow-Origin'] = '*'
     return res
